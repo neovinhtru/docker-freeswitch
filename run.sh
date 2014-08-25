04f452d72a51a8f234cb25d605bf6173234ef7f1
neovinhtru <vutuananh5@gmail.com>
2014-08-25 13:50:51 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#!/bin/sh
-
-# switch the subshell to the tomcat directory so that any relative
-# paths specified in any configs are interpreted from this directory.
-
-/opt/freeswitch/bin/freeswitch -ncwait
-
-echo '222222'
